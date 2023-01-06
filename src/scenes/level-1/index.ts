dbe5ca17831fc28b04d0756c939c29264b7cdb02
Joseph Elias <jrelias@outlook.com>
2023-01-06 03:41:20 -0500

    set up player class and movement, loaded onto level 1

--- src/scenes/level-1/index.ts
+++ src/scenes/level-1/index.ts
@@ -1,10 +1,16 @@
 import { Scene, GameObjects } from 'phaser';
+import { Player } from '../../classes/player';
 export class Level1 extends Scene {
-  private king!: GameObjects.Sprite;
+  // private king!: GameObjects.Sprite;
+  private player!: Player;
   constructor() {
     super('level-1-scene');
   }
   create(): void {
-    this.king = this.add.sprite(100, 100, 'king');
+    // this.king = this.add.sprite(100, 100, 'king');
+    this.player = new Player(this, 100, 100);
+  }
+  update(): void {
+    this.player.update();
   }
 }
